4536b90d8c7f7a73cefb49fcab55ae5783df8818
Azamat Tadeev <azamat.tadeev@mail.ru>
2020-09-21 20:21:31 +0300

    Fix problem with Invalid certificate on developerslife.ru. App's using unsafe OkHttpClient noew

--- app/src/main/java/com/example/hw4_devlife/loaders/PostsLoader.java
+++ app/src/main/java/com/example/hw4_devlife/loaders/PostsLoader.java
@@ -17,9 +17,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.net.URL;
+import java.security.cert.CertificateException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -28,7 +36,7 @@ import okhttp3.Response;
 public class PostsLoader extends AsyncTask<Integer, Void, Void> {
     private OnLoadPostsListener listener;
     private WeakReference<PreviewsActivity> activityReference;
-    private static final OkHttpClient client = new OkHttpClient();
+    private static final OkHttpClient client = getUnsafeOkHttpClient();
 
     public static final int POSTS_NUMBER_AT_ONE_TIME = 5;
 
@@ -84,8 +92,51 @@ public class PostsLoader extends AsyncTask<Integer, Void, Void> {
     private String getUrl(int page) {
         return String.format(
                 Locale.getDefault(),
-                "https://developerslife.ru/latest/%d?json=true",
+                "http://developerslife.ru/latest/%d?json=true",
                 page);
     }
+
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
